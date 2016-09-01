cd75bcc311900c1a3012997f39e80420f5ec5c0e
XeliteXirish <shaunoneill7@me.com>
2016-09-01 18:23:19 +0100

    Workkk

--- app/src/main/java/com/xelitexirish/tcdgandroidapp/ui/MembersFragment.java
+++ app/src/main/java/com/xelitexirish/tcdgandroidapp/ui/MembersFragment.java
@@ -1,7 +1,22 @@
 package com.xelitexirish.tcdgandroidapp.ui;
 
-/**
- * Created by Shaun on 24/08/2016.
- */
-public class MembersFragment {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MembersFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
